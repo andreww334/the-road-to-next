1b25c12bc575e251dd7f8b7797fe8886c8aa0b40
Andrew Wang <wangandrw330@gmail.com>
2025-03-24 16:14:02 -0500

    styling module

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
+import { ticketsPath } from "@/paths";
+
 const HomePage = () => {
   return (
-    <div>
-      <h2>Home Page!</h2>
+    <div className="flex-1 flex flex-col gap-y-8">
+      <div>
+        <h2 className="text-3xl font-bold tracking-tight">Home Page!!!</h2>
+        <p className="text-sm text-muted-foreground">
+          {" "}
+          Your home page to start
+        </p>
+      </div>
+      <div className="flex-1 flex flex-col items-center">
+        <Link href={ticketsPath()} className="underline">
+          Go to tickets
+        </Link>
+      </div>
     </div>
   );
 };
