2c7e37fa29c3d6cd2e6fd4c4e39ab52424ceb2e7
Andrew Wang <wangandrw330@gmail.com>
2025-03-25 17:58:54 -0500

    lint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,6 @@
 import "./globals.css";
-import { LucideKanban } from "lucide-react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Link from "next/link";
-import { buttonVariants } from "@/components/ui/button";
-import { homePath, ticketsPath } from "@/paths";
 import { Header } from "@/components/Header";
 
 const geistSans = Geist({
