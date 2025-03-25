a349dbf39950ede048dee7b8af232359503e9c0c
Andrew Wang <wangandrw330@gmail.com>
2025-03-25 11:10:12 -0500

    shadcn module

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,54 +1,20 @@
-import clsx from "clsx";
+import { LucideCircleCheck, LucideFileText, LucidePencil } from "lucide-react";
 import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
 import { initialTickets } from "@/data";
 import { ticketPath } from "@/paths";
-const CheckIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="currentColor"
-    className="size-6"
-  >
-    <path
-      fillRule="evenodd"
-      d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
-      clipRule="evenodd"
-    />
-  </svg>
-);
-
-const PencilIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="currentColor"
-    className="size-6"
-  >
-    <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z" />
-  </svg>
-);
-
-const DocumentIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth={1.5}
-    stroke="currentColor"
-    className="size-6"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
-    />
-  </svg>
-);
 
 const TICKET_ICONS = {
-  OPEN: <PencilIcon />,
-  DONE: <CheckIcon />,
-  IN_PROGRESS: <DocumentIcon />,
+  OPEN: <LucideFileText />,
+  DONE: <LucideCircleCheck />,
+  IN_PROGRESS: <LucidePencil />,
 };
 
 const TicketsPage = () => {
@@ -60,30 +26,33 @@ const TicketsPage = () => {
           All your tickets in one place
         </p>
       </div>
+      <Separator />
       <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
         {initialTickets.map((ticket) => {
           return (
-            <div
-              key={ticket.id}
-              className="w-full max-w-[420px] p-4 border border-slate-100 rounded"
-            >
-              <div> {TICKET_ICONS[ticket.status]} </div>
-              <h3 className="text-lg font-semibold truncate">{ticket.title}</h3>
-              <p
-                className={clsx("text-sm text-slate-500 truncate", {
-                  "line-through": ticket.status === "DONE",
-                })}
-              >
-                {ticket.content}
-              </p>
-
-              <Link
-                href={ticketPath(ticket.id.toString())}
-                className="underline"
-              >
-                View
-              </Link>
-            </div>
+            <Card key={ticket.id} className="w-full max-w-[420px]">
+              <CardHeader>
+                <CardTitle className="flex gap-x-2">
+                  <span className="flex items-center">
+                    {TICKET_ICONS[ticket.status]}
+                  </span>
+                  <h3 className=" text-2xl truncate">{ticket.title}</h3>
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <span className="line-clamp-3 whitespace-break-spaces">
+                  {ticket.content}
+                </span>
+              </CardContent>
+              <CardFooter>
+                <Link
+                  href={ticketPath(ticket.id.toString())}
+                  className="underline"
+                >
+                  View
+                </Link>
+              </CardFooter>
+            </Card>
           );
         })}
       </div>
