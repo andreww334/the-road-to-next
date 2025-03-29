7fa916b010a405903539314313301dd7d51caae8
Andrew Wang <wangandrw330@gmail.com>
2025-03-29 00:52:36 -0500

    server actions

--- src/features/ticket/queries/get-ticket.ts
+++ src/features/ticket/queries/get-ticket.ts
@@ -1,5 +1,5 @@
+import { Ticket } from "@prisma/client";
 import { prisma } from "@/lib/prisma";
-import { Ticket } from "../types";
 
 export const getTicket = async (ticketId: string): Promise<Ticket | null> => {
   return await prisma.ticket.findUnique({
