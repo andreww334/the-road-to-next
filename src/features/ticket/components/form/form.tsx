76ca0fa9ec73c5f4539fbb7f46bed0f033a08b68
Andrew Wang <wangandrw330@gmail.com>
2025-03-30 23:30:38 -0500

    form abstraction

---
+++ src/features/ticket/components/form/form.tsx
@@ -0,0 +1,34 @@
+import { toast } from "sonner";
+import { useActionFeedback } from "./hooks/use-action-feedback";
+import { ActionState } from "./utils/to-action-state";
+
+type FormProps = {
+  action: (payload: FormData) => void;
+  actionState: ActionState;
+  children: React.ReactNode;
+};
+
+const Form = ({ action, actionState, children }: FormProps) => {
+  useActionFeedback(actionState, {
+    onSuccess: ({ actionState }) => {
+      if (actionState.message) {
+        console.log(`successful: ${actionState.message}`);
+        toast.success(actionState.message);
+      }
+    },
+    onError: ({ actionState }) => {
+      if (actionState.message) {
+        console.log(`error: ${actionState.message}`);
+        toast.error(actionState.message);
+      }
+    },
+  });
+
+  return (
+    <form action={action} className="flex flex-col gap-y-2">
+      {children}
+    </form>
+  );
+};
+
+export { Form };
--- src/features/ticket/components/ticket-upsert-form.tsx
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -2,13 +2,12 @@
 
 import { Ticket } from "@prisma/client";
 import { useActionState } from "react";
-import { toast } from "sonner";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { upsertTicket } from "../actions/upsert-ticket";
-import { FieldError } from "./field-error";
-import { useActionFeedback } from "./form/hooks/use-action-feedback";
+import { FieldError } from "./form/field-error";
+import { Form } from "./form/form";
 import { SubmitButton } from "./form/submit-button";
 import { EMPTY_ACTION_STATE } from "./form/utils/to-action-state";
 
@@ -21,23 +20,8 @@ const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
     EMPTY_ACTION_STATE
   );
 
-  useActionFeedback(actionState, {
-    onSuccess: ({ actionState }) => {
-      if (actionState.message) {
-        console.log(`successful: ${actionState.message}`);
-        toast.success(actionState.message);
-      }
-    },
-    onError: ({ actionState }) => {
-      if (actionState.message) {
-        console.log(`error: ${actionState.message}`);
-        toast.error(actionState.message);
-      }
-    },
-  });
-
   return (
-    <form action={action} className="flex flex-col gap-y-2">
+    <Form action={action} actionState={actionState}>
       <Label htmlFor="title">Title</Label>
       <Input
         id="title"
@@ -64,7 +48,7 @@ const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
       <FieldError actionState={actionState} name="content" />
       <SubmitButton label={ticket ? "Edit" : "Create"} />
       {actionState.message}
-    </form>
+    </Form>
   );
 };
 
