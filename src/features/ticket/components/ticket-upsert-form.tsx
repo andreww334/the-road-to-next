ba256ad19d430038d301724c0ed47ee8297c6fc1
Andrew Wang <wangandrw330@gmail.com>
2025-03-30 21:28:55 -0500

    action callback handlers

--- src/features/ticket/components/ticket-upsert-form.tsx
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -7,6 +7,7 @@ import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { upsertTicket } from "../actions/upsert-ticket";
 import { FieldError } from "./field-error";
+import { useActionFeedback } from "./form/hooks/use-action-feedback";
 import { SubmitButton } from "./form/submit-button";
 import { EMPTY_ACTION_STATE } from "./form/utils/to-action-state";
 
@@ -18,6 +19,15 @@ const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
     upsertTicket.bind(null, ticket?.id),
     EMPTY_ACTION_STATE
   );
+
+  useActionFeedback(actionState, {
+    onSuccess: ({ actionState }) => {
+      console.log(actionState.message);
+    },
+    onError: ({ actionState }) => {
+      console.log(actionState.message);
+    },
+  });
 
   return (
     <form action={action} className="flex flex-col gap-y-2">
