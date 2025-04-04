8c43ae82a42bd6b4909607409c4fccf69ea02d24
Andrew Wang <wangandrw330@gmail.com>
2025-04-04 11:28:34 -0700

    header customized by signed in/out

--- src/features/auth/actions/sign-out.ts
+++ src/features/auth/actions/sign-out.ts
@@ -2,9 +2,9 @@
 
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+import { getAuth } from "@/features/auth/queries/get-auth";
 import { lucia } from "@/lib/lucia";
 import { signInPath } from "@/paths";
-import { getAuth } from "../queries/get-auth";
 
 export const signOut = async () => {
   const { session } = await getAuth();
