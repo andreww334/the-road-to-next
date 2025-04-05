3b474c0dadcbaf861fea8ccf5ade15a48bcc168d
Andrew Wang <wangandrw330@gmail.com>
2025-04-04 19:47:01 -0700

    auth on tickets page

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,12 +1,11 @@
 import { Suspense } from "react";
 import { CardCompact } from "@/components/card-compact";
 import { Heading } from "@/components/Heading";
-import {} from "@/components/redirect-toast";
 import Spinner from "@/features/ticket/components/spinner";
 import { TicketList } from "@/features/ticket/components/ticket-list";
 import { TicketUpsertForm } from "@/features/ticket/components/ticket-upsert-form";
 
-const TicketsPage = () => {
+const TicketsPage = async () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets at one place" />
---
+++ src/features/auth/queries/get-auth-or-redirect.ts
@@ -0,0 +1,10 @@
+import { redirect } from "next/navigation";
+import { signInPath } from "@/paths";
+import { getAuth } from "./get-auth";
+
+export const getAuthOrRedirect = async () => {
+  const { user } = await getAuth();
+  if (!user) {
+    redirect(signInPath());
+  }
+};
