5cefb62c8e3885b3d824bfd72c7151a49a0d1204
Andrew Wang <wangandrw330@gmail.com>
2025-04-05 00:41:09 -0700

    add auth to all server ticket update/delete actions

--- src/features/ticket/queries/get-tickets.ts
+++ src/features/ticket/queries/get-tickets.ts
@@ -1,7 +1,6 @@
-import { Ticket } from "@prisma/client";
 import { prisma } from "@/lib/prisma";
 
-export const getTickets = async (): Promise<Ticket[]> => {
+export const getTickets = async () => {
   return await prisma.ticket.findMany({
     orderBy: {
       createdAt: "desc",
