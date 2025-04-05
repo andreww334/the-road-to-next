8f522941e8c2428a6aea4b8bec37aefdf7e2ff86
Andrew Wang <wangandrw330@gmail.com>
2025-04-05 12:11:15 -0700

    sidebar only on authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,11 +37,14 @@ export default function RootLayout({
             <Sidebar />
             <main
               className="
+              duration-200
+              pl-[78px]
+              peer-hover:pl-[240px]
               min-h-screen flex-1
-            overflow-y-auto overflow-x-hidden
-            py-24 px-8
-            bg-secondary/20
-            flex flex-col"
+              overflow-y-auto overflow-x-hidden
+              py-24 px-8
+              bg-secondary/20
+              flex flex-col"
             >
               {children}
             </main>
